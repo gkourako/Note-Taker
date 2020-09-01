330ccf002a04cf63c20394f9963ba36bc5ef4e8e
gkourako <georgekourakos4@gmail.com>
2020-09-01 17:19:57 -0400

    Added get, post, and delete. Application is not working yet

--- Develop/server.js
+++ Develop/server.js
@@ -13,6 +13,25 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+  return res.json(characters);
+});
+
+app.post("/api/notes", function(req, res){
+  // var newNote = 
+  console.log(req.body)
+});
+
+app.delete("/api/notes/:id", function(req, res){
+
+});
+
+app.get("*", function(req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Last thing
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
